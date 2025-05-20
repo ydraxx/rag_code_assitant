34aebd3360b9407345275656432eec875ed4e6d3
Corentin <corentin.madec@epitech.eu>
2025-05-20 14:45:46 +0200

    upd

--- app/home.py
+++ app/home.py
@@ -1,13 +1,15 @@
 import streamlit as st
 from streamlit.components.v1 import html
 
+from functions_llm_request import LLM_request
+
 
 st.title('AIDoc')
 user_query = st.text_area('Enter code snippet:')
 
 if st.button('Ask.'):
     if user_query:
-        response = LLM_request(user_query)
+        response = LLM_request(query=user_query)
         st.write
     else:
         st.write('Type a request.')
